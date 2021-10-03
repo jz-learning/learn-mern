4aa22247d5cc4aa4afbe6c8d442c3110b27070a8
James Zhang <jameszhanga@gmail.com>
2021-10-02 21:31:26 -0500

    Build two custum APIs

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,12 +1,43 @@
 import express from 'express';
 
+import { User } from '../database/models';
+import sha256 from 'sha256';
+
 const userController = express.Router();
 
+/**
+ * GET/
+ * retrieve and display all Users in the User Model
+ */
 userController.get('/', (req, res) => {
-  res.status(200).json({
-    status : 'success',
-    hello  : 'james'
+  User.find({}, (err, result) => {
+    res.status(200).json({
+      data : result
+    });
   });
+});
+
+/**
+ * POST/
+ * Add a new User to your database
+ */
+userController.post('/add-user', (req, res) => {
+  const { email, password } = req.body;
+
+  const userData = {
+    email,
+    hashedPassword : sha256(password)
+  };
+
+  const newUser = new User(data);
+  newUser
+    .save()
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(400), send('unable to save to database');
+    });
 });
 
 export default userController;
